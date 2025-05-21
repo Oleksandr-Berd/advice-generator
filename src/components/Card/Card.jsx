d462c9afbcba2400f9defe292eeb9015ccb02d83
Oleksandr-Berd <alex.berd86@gmail.com>
2025-05-21 21:48:30 +0300

    desktop done

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,18 +1,27 @@
-import patternMob from "../../assets/images/pattern-divider-mobile.svg"
-import dice from "../../assets/images/icon-dice.svg"
+import patternMob from "../../assets/images/pattern-divider-mobile.svg";
+import patternDesk from "../../assets/images/pattern-divider-desktop.svg";
 
-import * as SC from "./CardStyled"
+import dice from "../../assets/images/icon-dice.svg";
+
+import * as SC from "./CardStyled";
+import { useScreenSize } from "../../hooks/useSizeScreen";
 const Card = () => {
-    return ( 
-        <SC.CardCustom>
-            <h3>advice # 117</h3>
-            <p>“It is easy to sit up and take notice, what's difficult is getting up and taking action.”</p>
-            <img src={patternMob} alt="divider" />
-            <SC.DiceCustom>
-            <img src={dice} alt="dice" />
-            </SC.DiceCustom>
-        </SC.CardCustom>
-     );
-}
- 
+  const { isDesktop } = useScreenSize();
+
+  return (
+    <SC.CardCustom>
+      <h3>advice # 117</h3>
+      <p>
+        “It is easy to sit up and take notice, what's difficult is getting up
+        and taking action.”
+      </p>
+      {isDesktop ? <img src={patternDesk} alt="divider" /> : <img src={patternMob} alt="divider" />}
+
+      <SC.DiceCustom>
+        <img src={dice} alt="dice" />
+      </SC.DiceCustom>
+    </SC.CardCustom>
+  );
+};
+
 export default Card;
--- src/components/Card/CardStyled.js
+++ src/components/Card/CardStyled.js
@@ -1,7 +1,7 @@
 import styled from "@emotion/styled";
 
 export const CardCustom = styled.div`
-position: relative;
+  position: relative;
 
   display: inline-block;
 
@@ -17,44 +17,74 @@ position: relative;
   color: #cee3e9;
   border-radius: 10px;
 
+  @media (min-width: 1280px) {
+    margin: 0;
+    padding-left: 48px;
+    padding-right: 48px;
+  }
+
   & > h3 {
     margin-bottom: 24px;
 
     text-transform: uppercase;
 
     font-size: 11px;
+    font-family: "Manrope";
     font-weight: bolder;
     color: #53ffaa;
+    letter-spacing: 3.46px;
+
+    @media (min-width: 1280px) {
+      font-size: 13px;
+    }
   }
 
   & > p {
     margin-bottom: 24px;
 
     font-size: 24px;
+    font-family: "Manrope";
     font-weight: bolder;
     letter-spacing: -0.26px;
+
+    @media (min-width: 1280px) {
+      width: 400px;
+
+      margin-bottom: 40px;
+      margin-left: auto;
+      margin-right: auto;
+
+      font-size: 28px;
+    }
   }
 `;
 
 export const DiceCustom = styled.div`
-position: absolute;
+  position: absolute;
 
-left: 50%;
-bottom: 0;
+  left: 50%;
+  bottom: 0;
 
-transform: translateX(-50%) translateY(50%);
+  transform: translateX(-50%) translateY(50%);
 
-width: 64px;
-height: 64px;
+  width: 64px;
+  height: 64px;
 
-align-content: center;
+  align-content: center;
 
-background-color: #53FFAA;
+  background-color: #53ffaa;
 
-border-radius: 50%;
+  border-radius: 50%;
+transition: box-shadow 0.3s linear;
 
-& > img{
+&:active, &:hover, &:focus{
+    box-shadow: 0 0 10px #53ffaa, 0 0 20px #53ffaa88, 0 0 30px #53ffaa66;
+transition: box-shadow 0.3s linear;
+    cursor: pointer;
+}
+
+  & > img {
     margin-left: auto;
     margin-right: auto;
-}
-`
+  }
+`;
